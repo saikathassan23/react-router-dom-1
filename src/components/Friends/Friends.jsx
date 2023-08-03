be7d3676d103a7b8a804ad6a3bbb9f1cad3de31f
saikat <saikathassan23@gmail.com>
2023-08-04 01:38:36 +0600

    added more info

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -9,7 +9,13 @@ const Friends = () => {
     <div className={`container p-4 grid grid-cols-3`}>
       {navigation.state === 'loading' ? (
         // <h1>Loading...</h1>
-        <img src='https://i.gifer.com/ZKZg.gif' className='w-1/2' alt='loader' />
+        <div className='flex items-center justify-center w-[400px] h-[400px] mx-auto'>
+          <img
+            src='https://i.gifer.com/ZKZg.gif'
+            className='w-1/2'
+            alt='loader'
+          />
+        </div>
       ) : (
         data.map((data) => <Friend key={data.id} data={data} />)
       )}
