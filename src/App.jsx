a3250cf08ea4c1c507e8e305273924cb0428817a
saikat <saikathassan23@gmail.com>
2023-08-04 01:16:08 +0600

    spinner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 // import './App.css';
 import Header from './components/Header/Header';
 
 function App() {
+  const navigation = useNavigation();
   return (
     <>
       <Header />
+      {/* <div>{navigation.state === 'loading' ? <h1>Loading...</h1> : ''}</div> */}
       <Outlet />
     </>
   );
--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,14 +1,18 @@
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigation } from 'react-router-dom';
 import Friend from '../Friend/Friend';
 
 const Friends = () => {
   const data = useLoaderData();
+  const navigation = useNavigation();
+  console.log(navigation);
   return (
-    <div className='container p-4 grid grid-cols-3'>
-      {/* <h1>My Friends {Array.isArray(data).toString()}</h1> */}
-      {data.map((data) => (
-        <Friend key={data.id} data={data} />
-      ))}
+    <div className={`container p-4 grid grid-cols-3`}>
+      {navigation.state === 'loading' ? (
+        // <h1>Loading...</h1>
+        <img src='https://i.gifer.com/ZKZg.gif' className='w-1/2' alt='loader' />
+      ) : (
+        data.map((data) => <Friend key={data.id} data={data} />)
+      )}
     </div>
   );
 };
